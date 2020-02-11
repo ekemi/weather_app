76864de52f475b988f11c9507c0d21b5d1d1ecd7
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-02-11 22:34:29 +0000

    getting started

---
+++ lib/cli.rb
@@ -0,0 +1,5 @@
+class WeatherApp::CLI
+  def call
+    puts 'hi'
+  end
+end
--- lib/weather_app.rb
+++ lib/weather_app.rb
@@ -1,5 +1,8 @@
-require "weather_app/version"
+require 'bundler/setup'
+Bundler.require(:default, :development)
 
+require_relative "./weather_app/version"
+require_relative './cli'
 module WeatherApp
   class Error < StandardError; end
   # Your code goes here...
